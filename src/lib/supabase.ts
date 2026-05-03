ff70c2b1343f85f9e4b1e1409b1ecc6c12bd172f
njanuzzi <nubiajanuzzicontato@gmail.com>
2026-05-03 02:20:22 -0300

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './database.types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ojmaxsskczukdbxpaull.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qbWF4c3NrY3p1a2RieHBhdWxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3NjczNTEsImV4cCI6MjA5MzM0MzM1MX0.CL9IxI31OrDcZuTBh711TQjb8A5Ep0jveG7df58-dws';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
